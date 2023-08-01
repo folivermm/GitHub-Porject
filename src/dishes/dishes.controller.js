a2a4baa919e26b9007011da256f000ce7495723d
Oliver Magrane <foliverm@gmail.com>
2023-08-01 15:23:00 -0700

    dishes-controller

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -1,9 +1,86 @@
 const path = require("path");
-
-// Use the existing dishes data
 const dishes = require(path.resolve("src/data/dishes-data"));
-
-// Use this function to assign ID's when necessary
 const nextId = require("../utils/nextId");
 
-// TODO: Implement the /dishes handlers needed to make the tests pass
+//middleware to check for dish existance 
+function getDishById(req, res, next) {
+    const { dishId } = req.params;
+    const foundDish = dishes.find((dish) => dish.id === dishId);
+    if (foundDish) {
+        res.locals.dish = foundDish;
+        next();
+    } else {
+        next({ status: 404, message: `dish does net exist ${dishId}` })
+    }
+
+}
+
+//middlware for validading dishes properties
+function validateDish(req, res, next) {
+    const { data: { name, description, price, image_url } = {} } = req.body;
+    if (!name || name === "") {
+        next({ status: 400, message: "Dish must include a name" })
+    }
+    if (!description || description === "") {
+        next({ status: 400, message: "Dish must include a description" })
+    }
+    if (!price) {
+        next({ status: 400, message: "Dish must include a price" })
+    }
+    if (price <= 0 || !Number.isInteger(price)) {
+        next({ status: 400, message: "Dish must have a price that is an integer greater than 0" })
+    }
+    if (!image_url || image_url === "") {
+        next({ status: 400, message: "Dish must include a image_url" })
+    }
+    next();
+}
+
+//middleware to check dish id matches route id 
+function dishIdMatch(req, res, next) {
+    if (!req.body.data.id) {
+        next();
+    }
+    if (req.body.data.id !== req.params.dishId) {
+        next({ status: 400, message: `Dish id: ${req.body.data.id} does not match route id: ${req.params.dishId}` });
+    }
+    next();
+}
+
+//route handlers create, read, update, list
+function create(req, res, next) {
+    const { data: { id, name, description, price, image_url } = {} } = req.body;
+    const newDish = {
+        id: nextId(),
+        name,
+        description,
+        price,
+        image_url,
+    };
+    dishes.push(newDish);
+    res.status(201).json({ data: newDish });
+}
+
+function read(req, res, next) {
+    res.json({ data: res.locals.dish })
+}
+
+function update(req, res, next) {
+    const { name, description, price, image_url } = req.body.data;
+    const { dish } = res.locals;
+    dish.name = name;
+    dish.description = description;
+    dish.price = price;
+    dish.image_url = image_url;
+    res.json({ data: dish })
+}
+
+function list(req, res, next) {
+    res.json({ data: dishes })
+}
+module.exports = {
+    list,
+    create: [validateDish, create],
+    read: [getDishById, read],
+    update: [getDishById, validateDish, dishIdMatch, update],
+}
